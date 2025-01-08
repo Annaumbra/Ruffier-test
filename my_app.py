0edb85ac11e9ff86f6fd4c18e615df40389ea38c
Annaumbra <anna.ben1@icloud.com>
2025-01-08 17:49:36 +0000

    my_app.py and instr.py create

---
+++ my_app.py
@@ -0,0 +1,10 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+
+
+from instr import *
+# from second_win import *

155e1008d22bd6fb71c6cc129eaf3f1fe7986aad
Annaumbra <anna.ben1@icloud.com>
2025-01-08 18:35:56 +0000

    final_win.py finished

--- final_win.py
+++ final_win.py
@@ -1,24 +1,35 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-       QApplication, QWidget,
-       QHBoxLayout, QVBoxLayout,
-       QGroupBox, QRadioButton,
-       QPushButton, QLabel, QListWidget, QLineEdit)
-
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
 
 from instr import *
 
+
 class FinalWin(QWidget):
-       def __init__(self):
-              super().__init__()
-       def initUI(self):
-              pass
-       def next_click(self):
-              pass
-       def connects(self):
-              pass
-       def set_appear(self):
-              pass
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.set_appear()
+        self.show()
+
+    def initUI(self):
+        self.workh_text = QLabel(txt_workheart)
+        self.index_text = QLabel(txt_index)
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.index_text, alignment=Qt.AlignCenter)
+        self.layout_line.addWidget(self.workh_text, alignment=Qt.AlignCenter)
+        self.setLayout(self.layout_line)
+
+    def set_appear(self):
+        self.setWindowTitle(txt_finalwin)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+
 app = QApplication([])
 fw = FinalWin()
 app.exec_()
